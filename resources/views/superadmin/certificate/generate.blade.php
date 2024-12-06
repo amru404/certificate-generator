41f2c16fa6fb6adb42e55df15a58fb057c3c3792
amru404 <amru.azzam3@gmail.com>
2024-12-07 01:57:05 +0700

    add table template certificte

--- resources/views/superadmin/certificate/generate.blade.php
+++ resources/views/superadmin/certificate/generate.blade.php
@@ -18,7 +18,12 @@
     </div>
 @endif
 
-<form action="{{ route('superadmin.certificate.storeTemplate') }}" method="POST" enctype="multipart/form-data">
+
+<div class="card">
+    <div class="card-header">
+        <h6>Add Template Certificate</h6>
+    </div>
+<form action="{{ route('superadmin.certificate.storeTemplate') }}" method="POST" enctype="multipart/form-data" class="m-3">
         @csrf
 
         <div class="mb-3">
@@ -36,5 +41,47 @@
         <button type="submit" class="btn text-white" style="background-color:#2D3E50;">Submit</button>
     </form>
 
+</div>
+
+
+<div class="card mt-4">
+        <div class="card-header d-flex justify-content-between align-items-center">
+            <h4 class="mb-0">Data Template Certificate</h4>
+            <div class="btn-group">
+            </div>
+        </div>
+        <div class="card-body">
+            <table class="table table-bordered table-striped" id="dataTable" width="100%" cellspacing="0">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Preview Template</th>
+                    <th>Nama Template</th>
+                </tr>
+            </thead>
+            <tfoot>
+                <tr>
+                    <th>No</th>
+                    <th>Preview Template</th>
+                    <th>Nama Template</th>
+                </tr>
+            </tfoot>
+
+            <?php $number = 1; ?>
+
+            <tbody>
+                @foreach ($templateCertif as $TC)
+                <tr>
+                    <td>{{ $number }}</td>
+                    <?php $number++; ?>
+                    <td><img src="{{ asset('storage/' . $TC->preview) }}" alt="Logo Event" class="img-fluid" style="max-height: 100px;"></td>
+                    <td>{{$TC->name}}</td>  
+                </tr>
+                @endforeach
+
+            </tbody>
+            </table>
+        </div>
+    </div>
 
 @endsection('content')
