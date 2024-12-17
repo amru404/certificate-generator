b7163bf96716e268a2e4fe49fbc54bb21c65060c
yudawisma <yudadani05@gmail.com>
2024-12-17 21:55:18 +0700

    cakmenyalawi

--- resources/views/superadmin/certificate/add_template.blade.php
+++ resources/views/superadmin/certificate/add_template.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts_dashboard.app')
 @section('content')
+@if(session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+@endif
+
+@if(session('error'))
+    <div class="alert alert-danger">
+        {{ session('error') }}
+    </div>
+@endif
 
 <div class="container mt-4">
     <h1>Create Certificate Template</h1>
